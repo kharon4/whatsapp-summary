f2fe23ab343d77c3b78fc3a0c315f98b0bba0e18
Abhishek Khurana <abhishekkhurana12345@gmail.com>
2021-01-31 01:40:33 +0530

    created basic gui

--- res/WS.js
+++ res/WS.js
@@ -1,1 +1,39 @@
-console.log(window['sW'],window['gW']);
+console.log(window['sW'],window['gW']);//should be deleted
+
+
+let words = new Set();
+
+words.add('temp');
+
+
+const createWordDom = (word)=>{
+    const dom = document.createElement('div');
+    
+    const textDom = document.createElement('span');
+    textDom.innerText = word;
+    textDom.classList.add('wordName');
+
+    const regExDom = document.createElement('span');
+    regExDom.innerText = 'regEx';
+    regExDom.classList.add('regExName');
+
+
+    const remDom = document.createElement('img');
+    remDom.src = './res/cancel.svg';
+
+    dom.append(textDom,regExDom,remDom);
+
+    return dom;
+}
+
+
+const listDom = document.getElementById('wordList');
+
+const updateListDom = ()=>{
+    listDom.innerHTML = '';
+    words.forEach((val)=>{
+        listDom.append(createWordDom(val));
+    });
+}
+
+updateListDom();
